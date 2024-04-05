d4a0e586b73c0f19f2ffa8a17273387c3e4e5c17
steelbird <sharonsofi777@gmail.com>
2024-04-05 12:10:09 +0530

    Login Page XML

--- app/src/main/java/com/example/sb/MainActivity.java
+++ app/src/main/java/com/example/sb/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.sb;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
